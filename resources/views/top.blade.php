918c672b8c302f11b3d1a254cf43b7f4c94d553d
AkariKusano <150099054+AkariKusano@users.noreply.github.com>
2023-11-09 12:07:58 +0900

    Create top.blade.php

---
+++ resources/views/top.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<head>
+    <title>迷い猫</title>
+
+<body>
+    <header>
+        <a href="login">ログイン</a>
+    </header>
+
+    <h1>迷い猫ネットワークへようこそ</h1>
+
+    <!-- ログイン後表示コンテンツ
+<a href="catSearch">猫を探す</a>
+-->
+
+    <!-- 仮置き文章 -->
+    <p>私たちの活動について</p></br>
+    <p>簡潔に言うと迷子の猫を保護する人、</p>
+    <p> 見失った人のためのホームページです。</p></br>
+
+    <footer>
+        <a href="">お問い合わせ</a>
+    </footer>
